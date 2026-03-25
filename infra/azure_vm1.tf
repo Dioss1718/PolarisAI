587a1130a980b680c829dd4e28a4f32344033d72
Dnyaneshwari40 <kathednyaneshwari@gmail.com>
2026-03-25 17:43:06 +0530

    PolarisAI update azure_vm1

---
+++ infra/azure_vm1.tf
@@ -0,0 +1,12 @@
+
+# Terraform Infra
+# Node: azure_vm1
+# Time: 1774440786
+
+
+resource "null_resource" "azure_vm1" {
+  provisioner "local-exec" {
+    command = "echo default infra applied"
+  }
+}
+
