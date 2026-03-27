6c14239349a8d0754d7b90eaacad986309a23774
Dnyaneshwari40 <kathednyaneshwari@gmail.com>
2026-03-27 11:43:55 +0530

    PolarisAI update azure_storage1

---
+++ infra/azure_storage1.tf
@@ -0,0 +1,20 @@
+
+# Terraform Infra
+# Node: azure_storage1
+# Time: 1774592035
+
+```terraform
+resource "null_resource" "azure_storage1" {
+  triggers = {
+    node_id = "azure_storage1"
+    action  = "TERMINATE_FORCE"
+    reason  = "Policy Approved"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      az storage container delete --name azure_storage1 --account-name <account_name>
+    EOF
+  }
+}
+```
