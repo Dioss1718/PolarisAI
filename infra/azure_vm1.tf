6b1585eb207084ba4faff20d26117f19ac4f45c2
Dnyaneshwari40 <kathednyaneshwari@gmail.com>
2026-03-27 11:12:24 +0530

    PolarisAI update azure_vm1

---
+++ infra/azure_vm1.tf
@@ -0,0 +1,19 @@
+
+# Terraform Infra
+# Node: azure_vm1
+# Time: 1774590144
+
+```terraform
+resource "null_resource" "azure_vm1" {
+  triggers = {
+    node_id = "azure_vm1"
+    reason  = "Policy Approved"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      az vm resize --resource-group <resource_group_name> --name azure_vm1 --size Standard_DS2_v2
+    EOF
+  }
+}
+```
