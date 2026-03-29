4c25d287502985c4eb92a16c883f014d95969262
Dnyaneshwari40 <kathednyaneshwari@gmail.com>
2026-03-29 22:56:55 +0530

    PolarisAI update gcp_lb1

---
+++ infra/gcp_lb1.tf
@@ -0,0 +1,59 @@
+
+# Terraform Infra
+# Node: gcp_lb1
+# Time: 1774805215
+
+```terraform
+resource "google_compute_backend_service" "gcp_lb1" {
+  name          = "gcp-lb1"
+  port_name     = "http"
+  protocol      = "HTTP"
+  health_checks = [google_compute_health_check.gcp_lb1.name]
+
+  backend {
+    group = google_compute_instance_group_manager.gcp_lb1.instance_group
+  }
+
+  security_policy = google_compute_security_policy.gcp_lb1.name
+}
+
+resource "google_compute_security_policy" "gcp_lb1" {
+  name        = "gcp-lb1"
+  display_name = "gcp-lb1"
+
+  rule {
+    action = "ALLOW"
+    priority = 1000
+    match {
+      versioned_expr = "SRC_IP"
+      config {
+        src_service = "all_services"
+      }
+    }
+  }
+}
+
+resource "google_compute_health_check" "gcp_lb1" {
+  name               = "gcp-lb1"
+  check_interval_sec = 1
+  timeout_sec        = 1
+  tcp_health_check {
+    port = "80"
+  }
+}
+
+resource "google_compute_instance_group_manager" "gcp_lb1" {
+  name = "gcp-lb1"
+  version {
+    instance {
+      zone = "us-central1-a"
+      machine_type = "f1-micro"
+      boot_disk {
+        initialize_params {
+          image = "debian-cloud/debian-9"
+        }
+      }
+    }
+  }
+}
+```
