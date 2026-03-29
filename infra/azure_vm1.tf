71663ef6528f6bb49d669f314e003d332f6dd7fb
Dnyaneshwari40 <kathednyaneshwari@gmail.com>
2026-03-30 00:02:11 +0530

    PolarisAI update azure_vm1

---
+++ infra/azure_vm1.tf
@@ -0,0 +1,139 @@
+
+# Terraform Infra
+# Node: azure_vm1
+# Time: 1774809132
+
+```terraform
+resource "null_resource" "azure_vm1" {
+  triggers = {
+    node_id = "azure_vm1"
+    action  = "DOWNSIZE_MEDIUM"
+    reason  = "Policy Approved"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      az vm resize --name azure_vm1 --resource-group <resource_group_name> --size Standard_DS2_v2
+    EOF
+  }
+}
+```
+
+```terraform
+resource "null_resource" "azure_vm1" {
+  triggers = {
+    node_id = "azure_vm1"
+    action  = "DOWNSIZE_MEDIUM"
+    reason  = "Policy Approved"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      az vm resize --name azure_vm1 --resource-group <resource_group_name> --size Standard_DS2_v2
+    EOF
+  }
+}
+```
+
+```terraform
+resource "null_resource" "azure_vm1" {
+  triggers = {
+    node_id = "azure_vm1"
+    action  = "DOWNSIZE_MEDIUM"
+    reason  = "Policy Approved"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      az vm resize --name azure_vm1 --resource-group <resource_group_name> --size Standard_DS2_v2
+    EOF
+  }
+}
+```
+
+```terraform
+resource "null_resource" "azure_vm1" {
+  triggers = {
+    node_id = "azure_vm1"
+    action  = "DOWNSIZE_MEDIUM"
+    reason  = "Policy Approved"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      az vm resize --name azure_vm1 --resource-group <resource_group_name> --size Standard_DS2_v2
+    EOF
+  }
+}
+```
+
+```terraform
+resource "null_resource" "azure_vm1" {
+  triggers = {
+    node_id = "azure_vm1"
+    action  = "DOWNSIZE_MEDIUM"
+    reason  = "Policy Approved"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      az vm resize --name azure_vm1 --resource-group <resource_group_name> --size Standard_DS2_v2
+    EOF
+  }
+}
+```
+
+```terraform
+resource "null_resource" "azure_vm1" {
+  triggers = {
+    node_id = "azure_vm1"
+    action  = "DOWNSIZE_MEDIUM"
+    reason  = "Policy Approved"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      az vm resize --name azure_vm1 --resource-group <resource_group_name> --size Standard_DS2_v2
+    EOF
+  }
+}
+```
+
+```terraform
+resource "null_resource" "azure_vm1" {
+  triggers = {
+    node_id = "azure_vm1"
+    action  = "DOWNSIZE_MEDIUM"
+    reason  = "Policy Approved"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      az vm resize --name azure_vm1 --resource-group <resource_group_name> --size Standard_DS2_v2
+    EOF
+  }
+}
+```
+
+```terraform
+resource "null_resource" "azure_vm1" {
+  triggers = {
+    node_id = "azure_vm1"
+    action  = "DOWNSIZE_MEDIUM"
+    reason  = "Policy Approved"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      az vm resize --name azure_vm1 --resource-group <resource_group_name> --size Standard_DS2_v2
+    EOF
+  }
+}
+```
+
+```terraform
+resource "null_resource" "azure_vm1" {
+  triggers = {
+    node_id = "azure_vm1"
+    action  = "DOWNSIZE_MEDIUM"
+    reason
