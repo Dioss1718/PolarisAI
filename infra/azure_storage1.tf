42ebd0d75f0dc86567abe9f74585c4d78c100e03
Dnyaneshwari40 <kathednyaneshwari@gmail.com>
2026-03-29 23:07:33 +0530

    PolarisAI update azure_storage1

---
+++ infra/azure_storage1.tf
@@ -0,0 +1,22 @@
+
+# Terraform Infra
+# Node: azure_storage1
+# Time: 1774805853
+
+```terraform
+resource "null_resource" "azure_storage1" {
+  triggers = {
+    node_id = "azure_storage1"
+    action  = "TERMINATE_FORCE"
+    reason  = "Policy Approved"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      az vm stop --ids $(az vm list --query "[?name=='${null_resource.azure_storage1.name}'].id" --output tsv)
+      az vm deallocate --ids $(az vm list --query "[?name=='${null_resource.azure_storage1.name}'].id" --output tsv)
+      az vm delete --ids $(az vm list --query "[?name=='${null_resource.azure_storage1.name}'].id" --output tsv) --yes
+    EOF
+  }
+}
+```
