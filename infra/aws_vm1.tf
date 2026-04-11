38034daa14a20b932957fd551d81fc2bd9fe2566
Dnyaneshwari40 <kathednyaneshwari@gmail.com>
2026-04-11 22:57:02 +0530

    PolarisAI update aws_vm1

---
+++ infra/aws_vm1.tf
@@ -0,0 +1,37 @@
+
+# Terraform Infra
+# Node: aws_vm1
+# Time: 1775928422
+
+resource "null_resource" "aws_vm1" {
+  triggers = {
+    node_id = "aws_vm1"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      host        = "aws_vm1"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/aws_vm1_key")
+    }
+
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y unattended-upgrades",
+      "sudo unattended-upgrades -y --auto-upgrade",
+      "sudo apt update && sudo apt full-upgrade -y",
+      "sudo apt autoremove -y",
+      "sudo apt autoclean -y",
+      "sudo apt clean -y",
+      "sudo apt autoremove -y",
+      "sudo apt autoclean -y",
+      "sudo apt clean -y",
+      "sudo apt update",
+      "sudo apt install -y aws-secure-2.0",
+      "sudo aws-secure-2.0 --install",
+      "sudo aws-secure-2.0 --update",
+      "sudo aws-secure-2.0 --verify"
+    ]
+  }
+}
