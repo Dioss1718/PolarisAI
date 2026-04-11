3d2ae80437f1b8756dfe0b8aea1df7f203f5a8cf
Dnyaneshwari40 <kathednyaneshwari@gmail.com>
2026-04-11 22:38:49 +0530

    PolarisAI update aws_vm1

---
+++ infra/aws_vm1.tf
@@ -0,0 +1,26 @@
+
+# Terraform Infra
+# Node: aws_vm1
+# Time: 1775927329
+
+resource "null_resource" "aws_vm1" {
+  triggers = {
+    node_id = "aws_vm1"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      host        = "aws_vm1"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/aws_vm1_key")
+    }
+
+    script = <<-EOF
+      sudo apt update && sudo apt install -y unattended-upgrades
+      sudo unattended-upgrades -y --auto-upgrade
+      sudo apt update && sudo apt install -y aws-secure-locations
+      sudo aws secure-locations enable
+    EOF
+  }
+}
