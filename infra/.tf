ae64ca83b8cc9b1ba3aa1f8d2666518414a8ab68
Dnyaneshwari40 <kathednyaneshwari@gmail.com>
2026-04-12 11:07:43 +0530

    PolarisAI update

---
+++ infra/.tf
@@ -0,0 +1,20 @@
+
+# Terraform Infra
+# Node: 
+# Time: 1775972263
+
+resource "null_resource" "node" {
+  triggers = {
+    node_id = "NODE_ID"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "echo 'SECURE_PATCH: Node ${self.triggers.node_id} removed due to policy approval'"
+  }
+
+  provisioner "local-exec" {
+    when    = create
+    command = "echo 'SECURE_PATCH: Node ${self.triggers.node_id} created'"
+  }
+}
