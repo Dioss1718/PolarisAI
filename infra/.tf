b55793eae087b2d025512871303a0eb05842ccdc
Dnyaneshwari40 <kathednyaneshwari@gmail.com>
2026-04-12 12:04:00 +0530

    PolarisAI update

---
+++ infra/.tf
@@ -0,0 +1,21 @@
+
+# Terraform Infra
+# Node: 
+# Time: 1775975640
+
+```terraform
+resource "null_resource" "node_termination" {
+  triggers = {
+    node_id = "NODE_ID"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "echo 'TERMINATE_FORCE: Reason: Policy Approved' && echo 'Node ID: NODE_ID'"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+```
